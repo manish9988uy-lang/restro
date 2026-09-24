3104cbf757270453773c8789f6475fee190d1505
Mr.PR0C0D <122796514+9shankar805@users.noreply.github.com>
2026-09-25 00:23:21 +0545

    Fix 419 Page Expired: Enable trusted proxies in Laravel middleware and forward HTTPS headers in Nginx

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'superadmin' => \App\Http\Middleware\SuperAdminMiddleware::class,
         ]);
